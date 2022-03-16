6e05ee9d8138dbd116c3277bf716c30c5540e94a
Julia Garibaldi <julita.gari@gmail.com>
2022-03-16 00:34:57 -0300

    Refactor infrastructure
    As both functions don't really need a reference to self, move them
    outside of the Infrastructure implementation.

--- src/infrastructure.rs
+++ src/infrastructure.rs
@@ -28,8 +28,8 @@ impl Infrastructure {
         let response = match self.model.get_upstream_for(method, path) {
             Some(upstream) => {
                 // TODO: remove unwrap()
-                let upstream_uri = Uri::from_str(Infrastructure::absolute_url_for(upstream, path).as_str()).unwrap();
-                let headers = Infrastructure::headers_for(&request, upstream);
+                let upstream_uri = Uri::from_str(absolute_url_for(upstream, path).as_str()).unwrap();
+                let headers = headers_for(&request, upstream);
 
                 let mut upstream_request = Request::from(request);
                 *upstream_request.uri_mut() = upstream_uri;
@@ -51,18 +51,18 @@ impl Infrastructure {
 
         Ok(response)
     }
+}
 
-    fn absolute_url_for(upstream: &str, original_path: &str) -> String {
-        let mut absolute_url = String::from("http://");
-        absolute_url.push_str(upstream);
-        absolute_url.push_str(original_path);
-        absolute_url
-    }
+fn absolute_url_for(upstream: &str, original_path: &str) -> String {
+    let mut absolute_url = String::from("http://");
+    absolute_url.push_str(upstream);
+    absolute_url.push_str(original_path);
+    absolute_url
+}
 
-    fn headers_for(request: &Request<Body>, upstream: &str) -> HeaderMap {
-        let original_headers = request.headers();
-        let mut headers = original_headers.clone();
-        headers.insert(HOST, upstream.parse().unwrap());
-        headers
-    }
+fn headers_for(request: &Request<Body>, upstream: &str) -> HeaderMap {
+    let original_headers = request.headers();
+    let mut headers = original_headers.clone();
+    headers.insert(HOST, upstream.parse().unwrap());
+    headers
 }
