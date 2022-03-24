0a7dc3ca136e68d7fd1ed9816eb32173a9d786fd
jgaribaldi <julita.gari@gmail.com>
2022-03-24 17:42:48 -0300

    Add simple test for AlwaysFirstUpstreamStrategy

--- src/model/upstream_strategy.rs
+++ src/model/upstream_strategy.rs
@@ -20,3 +20,26 @@ impl UpstreamStrategy for AlwaysFirstUpstreamStrategy {
             .map(|upstream| String::from(upstream))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{AlwaysFirstUpstreamStrategy, Route};
+    use crate::model::upstream_strategy::UpstreamStrategy;
+
+    #[test]
+    fn should_return_first_in_two_calls() {
+        let route = Route::build(
+            "route1",
+            &["GET"],
+            &["uri1", "uri2"],
+            &["upstream1", "upstream2"],
+        );
+        let strategy = AlwaysFirstUpstreamStrategy::build();
+
+        let first_result = strategy.next_for(&route);
+        let second_result = strategy.next_for(&route);
+
+        assert_eq!(Some(String::from("upstream1")), first_result);
+        assert_eq!(Some(String::from("upstream1")), second_result);
+    }
+}
