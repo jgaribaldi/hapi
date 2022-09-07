ef83fe246271af226528bc5f9b19b14ccafb6ef9
Julia Garibaldi <julita.gari@gmail.com>
2022-09-07 00:53:37 -0300

    Small refactor in request processor
    For improved clarity in the code and lock gathering isolation in
    non-async function.

--- src/infrastructure/processor.rs
+++ src/infrastructure/processor.rs
@@ -19,39 +19,46 @@ pub async fn process_request(
     let method = request.method().to_string();
     let path = request.uri().path().to_string();
 
-    let upstream;
-    {
-        let mut ctx = context.lock().unwrap();
-        upstream = ctx.upstream_lookup(path.as_str(), method.as_str());
-    }
+    let maybe_upstream = search_upstream(context, path.as_str(), method.as_str());
+    let response = match maybe_upstream {
+        Some(upstream_address) => {
+            let upstream_uri = Uri::from_str(absolute_url_for(&upstream_address, path.as_str()).as_str())?;
+            let headers = headers_for(&request, &upstream_address);
 
-    let response = if let Some(ups) = upstream {
-        let upstream_uri = Uri::from_str(absolute_url_for(&ups, path.as_str()).as_str())?;
-        let headers = headers_for(&request, &ups);
+            let mut upstream_request = Request::from(request);
+            *upstream_request.uri_mut() = upstream_uri;
+            *upstream_request.headers_mut() = headers;
+            log::debug!("Generated: {:?}", &upstream_request);
 
-        let mut upstream_request = Request::from(request);
-        *upstream_request.uri_mut() = upstream_uri;
-        *upstream_request.headers_mut() = headers;
-        log::debug!("Generated: {:?}", &upstream_request);
+            stats::count_request(
+                stats,
+                client.as_str(),
+                method.as_str(),
+                path.as_str(),
+                upstream_address.to_string().as_str(),
+            )
+                .await;
 
-        stats::count_request(
-            stats,
-            client.as_str(),
-            method.as_str(),
-            path.as_str(),
-            ups.to_string().as_str(),
-        )
-        .await;
-
-        let client = Client::new();
-        client.request(upstream_request).await?
-    } else {
-        log::debug!("No routes found for {:?}", request);
-        Response::builder().status(404).body(Body::empty()).unwrap()
+            let client = Client::new();
+            client.request(upstream_request).await?
+        }
+        None => {
+            log::debug!("No routes found for {:?}", request);
+            Response::builder().status(404).body(Body::empty()).unwrap()
+        }
     };
 
     log::debug!("Response: {:?}", &response);
     Ok(response)
+}
+
+fn search_upstream(
+    context: Arc<Mutex<Context>>,
+    path: &str,
+    method: &str,
+) -> Option<UpstreamAddress> {
+    let mut ctx = context.lock().unwrap();
+    ctx.upstream_lookup(path, method)
 }
 
 fn absolute_url_for(upstream: &UpstreamAddress, original_path: &str) -> String {
