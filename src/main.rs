d88dbc70aaf6a838fd67cc3fe9666ec9469927b9
jgaribaldi <julita.gari@gmail.com>
2022-09-18 17:00:15 -0300

    Extract signal handler into an independent fn

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use hyper::server::conn::AddrStream;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Server};
 use tokio::sync::mpsc;
+use tokio::sync::mpsc::Sender;
 
 use crate::errors::HapiError;
 use crate::infrastructure::processor;
@@ -35,7 +36,7 @@ async fn main() -> Result<(), HapiError> {
     let uph_thread_safe_context = thread_safe_context.clone();
 
     let thread_safe_stats = Arc::new(Mutex::new(Stats::build()));
-    let (main_cmd_tx, probe_handler_cmd_rx) = mpsc::channel(1024*size_of::<Command>());
+    let (main_cmd_tx, probe_handler_cmd_rx) = mpsc::channel(1024 * size_of::<Command>());
 
     // spawn upstream probe handler thread
     tokio::spawn(async move {
@@ -52,30 +53,6 @@ async fn main() -> Result<(), HapiError> {
         }
     }
 
-    // graceful quit handler (TODO: see how can we extract this as an independent function)
-    let graceful_quit_handler = async {
-        // let thread_safe_context = thread_safe_context.clone();
-        let gqh_cmd_tx = main_cmd_tx.clone();
-
-        tokio::signal::ctrl_c()
-            .await
-            .expect("Could not install graceful quit signal handler");
-
-        let mut upstream_addresses = Vec::new();
-        {
-            let ctx = gqh_thread_safe_context.lock().unwrap();
-            upstream_addresses.extend_from_slice(&ctx.get_all_upstreams().as_slice());
-        }
-
-        for ups in upstream_addresses.iter() {
-            match gqh_cmd_tx.send(Command::StopProbe { ups: ups.clone() }).await {
-                Ok(_) => log::debug!("Sent Probe command to probe handler for address {:?}", ups),
-                Err(error) => log::error!("Error sending message to probe handler {:?}", error),
-            }
-        }
-        log::info!("Shutting down Hapi. Bye :-)")
-    };
-
     let make_service = make_service_fn(move |conn: &AddrStream| {
         let context = thread_safe_context.clone();
         let stats = thread_safe_stats.clone();
@@ -91,7 +68,10 @@ async fn main() -> Result<(), HapiError> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let server = Server::bind(&addr)
         .serve(make_service)
-        .with_graceful_shutdown(graceful_quit_handler);
+        .with_graceful_shutdown(graceful_quit_handler(
+            main_cmd_tx.clone(),
+            gqh_thread_safe_context,
+        ));
 
     if let Err(e) = server.await {
         log::error!("server error: {}", e);
@@ -101,6 +81,32 @@ async fn main() -> Result<(), HapiError> {
 
 fn identify_client(remote_addr: &SocketAddr, _request: &Request<Body>) -> String {
     remote_addr.ip().to_string()
+}
+
+async fn graceful_quit_handler(
+    gqh_cmd_tx: Sender<Command>,
+    gqh_thread_safe_context: Arc<Mutex<Context>>,
+) {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("Could not install graceful quit signal handler");
+
+    let mut upstream_addresses = Vec::new();
+    {
+        let ctx = gqh_thread_safe_context.lock().unwrap();
+        upstream_addresses.extend_from_slice(&ctx.get_all_upstreams().as_slice());
+    }
+
+    for ups in upstream_addresses.iter() {
+        match gqh_cmd_tx
+            .send(Command::StopProbe { ups: ups.clone() })
+            .await
+        {
+            Ok(_) => log::debug!("Sent Probe command to probe handler for address {:?}", ups),
+            Err(error) => log::error!("Error sending message to probe handler {:?}", error),
+        }
+    }
+    log::info!("Shutting down Hapi. Bye :-)")
 }
 
 fn build_context_from_test_routes() -> Context {
