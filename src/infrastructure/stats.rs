cd1fe1fc308a6dab64b246aee0d94e4f43c34c14
jgaribaldi <julita.gari@gmail.com>
2022-11-13 12:31:29 -0300

    Remove unused code

--- src/infrastructure/stats.rs
+++ src/infrastructure/stats.rs
@@ -34,36 +34,6 @@ impl Stats {
         *self.counter.entry(key).or_insert(0) += 1;
     }
 
-    pub fn requests_by_client(&self, client: &str) -> Option<u64> {
-        self.counter
-            .iter()
-            .filter(|entry| {
-                // entries matching given client
-                let (cli, _, _, _) = entry.0;
-                *cli == client.to_string()
-            })
-            .map(|entry| entry.1.clone())
-            .reduce(|mut accum, value| {
-                accum += value;
-                return accum;
-            })
-    }
-
-    pub fn requests_by_upstream(&self, upstream: &str) -> Option<u64> {
-        self.counter
-            .iter()
-            .filter(|entry| {
-                // entries matching given upstream
-                let (_, _, _, ups) = entry.0;
-                *ups == upstream.to_string()
-            })
-            .map(|entry| entry.1.clone())
-            .reduce(|mut accum, value| {
-                accum += value;
-                return accum;
-            })
-    }
-
     pub fn get_all(&self) -> Vec<(String, String, String, String, u64)> {
         let mut result = Vec::new();
 
@@ -78,68 +48,5 @@ impl Stats {
         }
 
         result
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::infrastructure::stats::Stats;
-
-    #[test]
-    fn should_get_requests_by_client() {
-        // given:
-        let stats = sample_stats();
-
-        // when:
-        let result = stats.requests_by_client("client1").unwrap();
-
-        // then:
-        assert_eq!(2, result)
-    }
-
-    #[test]
-    fn should_get_requests_by_upstream() {
-        // given:
-        let stats = sample_stats();
-
-        // when:
-        let result = stats.requests_by_upstream("upstream2").unwrap();
-
-        // then:
-        assert_eq!(3, result)
-    }
-
-    #[test]
-    fn should_not_find_request_by_non_existent_client() {
-        // given:
-        let stats = sample_stats();
-
-        // when:
-        let result = stats.requests_by_client("client4");
-
-        // then:
-        assert_eq!(None, result)
-    }
-
-    #[test]
-    fn should_not_find_request_by_non_existent_upstream() {
-        // given:
-        let stats = sample_stats();
-
-        // when:
-        let result = stats.requests_by_upstream("upstream4");
-
-        // then:
-        assert_eq!(None, result)
-    }
-
-    fn sample_stats() -> Stats {
-        let mut stats = Stats::build();
-        stats.count_request("client1", "GET", "/path", "upstream1");
-        stats.count_request("client2", "GET", "/path", "upstream1");
-        stats.count_request("client1", "GET", "/path2", "upstream2");
-        stats.count_request("client2", "GET", "/path2", "upstream2");
-        stats.count_request("client3", "GET", "/path2", "upstream2");
-        stats
     }
 }
